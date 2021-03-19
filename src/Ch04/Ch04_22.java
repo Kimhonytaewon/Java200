6c14e50ed4b5a57de5ddb0bcd000ee3ead360391
Kimhonytaewon <kimhonytae.1@gmail.com>
2021-03-19 23:35:47 +0900

    2021.03.19

--- src/Ch04/Ch04_22.java
+++ src/Ch04/Ch04_22.java
@@ -1,6 +1,26 @@
 package Ch04;
 
+import java.util.Scanner;
+
 public class Ch04_22 {
     //기호 문자 "*"를 나열해서 직각의 이등변 삼각형을 표시하는 프로그램을 작성하자.
     //직각의 위치가 왼쪽 아래, 왼쪽 위, 오른쪽 아래, 오른쪽 위에 표시하는 프로그램을 각각 작성할 것
+    public static void main(String[] args) {
+        Scanner scanf = new Scanner(System.in);
+
+        //1. 왼쪽 아래가 직각
+        System.out.println("아랫변의 *갯수는?");
+        int a = scanf.nextInt();
+
+        for(int i = 1; i <= a; i++) {
+            for(int j = 1; i >= j; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------");
+
+        //2. 오른쪽 위가 직각
+
+    }
 }
--- src/Ch04/Ch04_23.java
+++ src/Ch04/Ch04_23.java
@@ -3,4 +3,7 @@ package Ch04;
 public class Ch04_23 {
     //n단 피라미드를 표시하는 프로그램을 작성하자. i행째에는 (i-1) * 2 + 1개의 "*"기호를 표시하고
     //마지막 행인 n행째에는 (n-1) * 2 + 1개의 "*"를 표시할 것
+    public static void main(String[] args) {
+
+    }
 }
